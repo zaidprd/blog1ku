fe427b7cbcb813cd2d2248065b8b76f467afc865
zaidprd <ahmadzaid.tamiang@gmail.com>
2025-09-24 16:23:55 +0700

    style: update website colors and footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
 // tailwind.config.mjs
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography';
-
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  // Tambahkan baris ini
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [typography],
-};
+  plugins: [],
+}
